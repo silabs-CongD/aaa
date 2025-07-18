431772d92fb0d924c4b6f65b7580688acc81e688
silabs-CongD <114218374+silabs-CongD@users.noreply.github.com>
2025-07-18 09:55:55 +0700

    Update a.sh

--- a.sh
+++ a.sh
@@ -1,7 +1,7 @@
 ls ~/zephyrproject/build/zephyr
 echo 0000000
-ls ~/zephyrproject/build/zephyr/CMakeFiles
-echo 0000000
-cat ~/zephyrproject/build/zephyr/zephyr_final.map
-echo 0000000
+ls ~/zephyrproject/build/zephyr/CMakeFiles/zephyr.dir
+echo 11111
+# cat ~/zephyrproject/build/zephyr/zephyr_final.map
+# echo 2222
 ls ~/zephyrproject/build

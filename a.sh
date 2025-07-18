802963798b361a514dcf3de7bcef440b39eb8b81
silabs-CongD <114218374+silabs-CongD@users.noreply.github.com>
2025-07-18 09:38:35 +0700

    Create a.sh

---
+++ a.sh
@@ -0,0 +1,7 @@
+ls ~/zephyrproject/build/zephyr
+echo 0000000
+ls ~/zephyrproject/build/zephyr/CMakeFiles
+echo 0000000
+cat ~/zephyrproject/build/zephyr/zephyr_final.map
+echo 0000000
+ls ~/zephyrproject/build

24bd8ce9135bf75eac2a8e2f93a53bfbe9f22576
silabs-CongD <114218374+silabs-CongD@users.noreply.github.com>
2025-07-21 18:14:31 +0700

    Update a.sh

--- a.sh
+++ a.sh
@@ -1,9 +1,17 @@
 ls ~/zephyrproject/build/zephyr
 echo 0000000
-ls ~/zephyrproject/build/zephyr/CMakeFiles/zephyr.dir
-echo 11111
-# cat ~/zephyrproject/build/zephyr/zephyr_final.map
+# ls ~/zephyrproject/build/zephyr/CMakeFiles/zephyr.dir
+# echo 11111
 
 ls ~/zephyrproject/build/CMakeFiles/app.dir/src
 echo 2222
+ls ~/zephyrproject/build/CMakeFiles/app.dir
+
+echo 33333333333333
+ls changed_projects
+
+# Find all .c and .cpp files and copy them to the folder
+find changed_projects -type f \( -name "*.c" -o -name "*.cpp" \) -exec cp -- "{}" ~/zephyrproject/build/CMakeFiles/app.dir/ \;
+
+echo 444444444444
 ls ~/zephyrproject/build/CMakeFiles/app.dir

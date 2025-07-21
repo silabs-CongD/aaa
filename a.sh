bd63fcd229ecaa35caa11f1825811c2284377f1e
silabs-CongD <114218374+silabs-CongD@users.noreply.github.com>
2025-07-21 18:52:56 +0700

    Update a.sh

--- a.sh
+++ a.sh
@@ -8,9 +8,11 @@ echo 2222
 ls ~/zephyrproject/build/CMakeFiles/app.dir
 
 echo 33333333333333
-ls $WORKSPACE/changed_projects
+mkdir tmp
+while read line; do cp -r $WORKSPACE/projects/applications/$line --parents ./tmp; \
+done < $WORKSPACE/projects/changed_projects.txt
 
-find $WORKSPACE/changed_projects -type f \( -name "*.c" -o -name "*.cpp" \) -exec cp -- "{}" ~/zephyrproject/build/CMakeFiles/app.dir/ \;
+find tmp -type f \( -name "*.c" -o -name "*.cpp" \) -exec cp -- "{}" ~/zephyrproject/build/CMakeFiles/app.dir/ \;
 
 echo 444444444444
 ls ~/zephyrproject/build/CMakeFiles/app.dir

e150b63728d6d40bda10d7629ddaa15546d46ea6
Silabs_CongD <Cong.Do@silabs.com>
2025-08-30 09:47:17 +0700

    new test

---
+++ projects/bluetooth/bluetooth_applications/gw/wifi/wifi_ble_gateway/autogen/sl_event_handler.c
@@ -0,0 +1,47 @@
+#include "sl_event_handler.h"
+
+
+void sli_driver_permanent_allocation(void)
+{
+}
+
+void sli_service_permanent_allocation(void)
+{
+}
+
+void sli_stack_permanent_allocation(void)
+{
+}
+
+void sli_internal_permanent_allocation(void)
+{
+}
+
+void sl_platform_init(void)
+{
+}
+
+void sli_internal_init_early(void)
+{
+}
+
+void sl_kernel_start(void)
+{
+}
+
+void sl_driver_init(void)
+{
+}
+
+void sl_service_init(void)
+{
+}
+
+void sl_stack_init(void)
+{
+}
+
+void sl_internal_app_init(void)
+{
+}
+
